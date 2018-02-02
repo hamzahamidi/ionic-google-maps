9549c8993a99d645e1c96419010d3523c2f97997
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-02-02 18:32:09 +0100

    Delete unused import
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
